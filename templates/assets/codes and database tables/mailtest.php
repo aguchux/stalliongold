ddfcc4216bf356f5962c59b8a0574070a5d617c4
aguchux <agu.chux@gmail.com>
2021-01-15 13:58:16 +0100

    Final update before marcel and izunna starts working on them

--- templates/assets/codes and database tables/mailtest.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-mail('$email', 'Hello', 'Hello, this is a test email', 'From: admin@domain.com');
-
-?>
--- templates/assets/connections/sessions.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-//If your session isn't valid, it returns you to the login screen for protection
-if(empty($_SESSION['user_id'])){
-	header("Location: login.php"); /*Redirect Browser*/
-	die;
-}
-?>
